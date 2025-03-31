83d2b6e439fe8bcc37ba9bb95e47ee33284bd5b4
vashistavallambhatla <vashista.vallambhatla@gmail.com>
2025-03-31 12:38:31 +0530

    module-8-completed

--- module8/http.go
+++ module8/http.go
@@ -7,28 +7,41 @@ import (
 	"context"
 )
 
-func ProcessHandler(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintf(w,"Starting the process")
+func LongRunningTask(done chan bool){
+	fmt.Println("Process started running..")
 	time.Sleep(5 * time.Second)
-	fmt.Fprintf(w,"Process completed")
+	fmt.Println("Process completed")
+	done <- true
+	close(done)
+}
+
+func ProcessHandler(w http.ResponseWriter, r * http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(),7 * time.Second)
+	defer cancel()
+	
+	done := make(chan bool) 
+	go LongRunningTask(done)
+
+	select { 
+		case <- done:
+			fmt.Fprintf(w,"Process completed successfully")
+		case <- ctx.Done():
+			if ctx.Err() == context.Canceled {
+				http.Error(w, "Request was cancelled by the client", http.StatusRequestTimeout)
+				fmt.Println("Request was cancelled by the client.")
+			} else if ctx.Err() == context.DeadlineExceeded{
+				http.Error(w,"Requqest timed out",http.StatusRequestTimeout)
+				fmt.Println("Request timed out")
+			}
+	}
 }  
 
 func main() {
-	ctx := context.Background()
-
-	ctxWithTimeOut, cancel := context.WithTimeout(ctx,5 * time.Second)
-	defer cancel()
-
 	http.HandleFunc("/process",ProcessHandler)
 
 	fmt.Println("Starting a server on port : 8080...") 
 
 	if err := http.ListenAndServe(":8080",nil); err != nil {
 		fmt.Println("Error occured while starting the server :",err)
-	}
-
-	select {
-		case <- ctxWithTimeOut.Done() :
-			fmt.Printf("The process ran for too long forcing main to quit")
 	}
 }
